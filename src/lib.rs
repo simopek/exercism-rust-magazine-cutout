5a44fdb3828ee3ed283b6a587c3d9d58cf9c4775
simopek <>
2022-04-07 22:53:55 +0200

    Format code

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,20 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-
     let mut magazine_words = HashMap::new();
 
     for word in magazine {
-        magazine_words.entry(word).and_modify(|e| { *e += 1 }).or_insert(1);
-    }    
+        magazine_words
+            .entry(word)
+            .and_modify(|e| *e += 1)
+            .or_insert(1);
+    }
 
     for note_word in note {
-
-        magazine_words.entry(note_word).and_modify(|e| { *e -= 1}).or_insert(-1);
-
+        magazine_words
+            .entry(note_word)
+            .and_modify(|e| *e -= 1)
+            .or_insert(-1);
     }
 
     magazine_words.values().all(|&x| x >= 0)
