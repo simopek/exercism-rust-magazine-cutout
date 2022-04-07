206e33c62db5aa42031b6927faa5faedde2c1d04
simopek <>
2022-04-07 22:50:55 +0200

    Improve code

--- src/lib.rs
+++ src/lib.rs
@@ -10,25 +10,13 @@ pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
 
     for word in magazine {
         magazine_words.entry(word).and_modify(|e| { *e += 1 }).or_insert(1);
-    }
+    }    
 
     for note_word in note {
-        let found = match magazine_words.get(note_word) {
-            None => false,
-            Some(&x) if x > 0 => {
-                magazine_words.entry(note_word).and_modify(|e| { *e -= 1});
-                true
-            },
-            _ => {
-                magazine_words.remove_entry(note_word);
-                false
-            }
-        };
 
-        if !found {
-            return false;
-        }
+        magazine_words.entry(note_word).and_modify(|e| { *e -= 1}).or_insert(-1);
+
     }
 
-    true
+    magazine_words.values().all(|&x| x >= 0)
 }
